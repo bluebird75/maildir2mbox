ebc8ab4f1f0bc846904d68c52209d963e98a0918
Philippe FREMY <phil.fremy@free.fr>
2020-04-15 22:22:47 +0200

    Update setup.py to actually work

--- setup.py
+++ setup.py
@@ -8,15 +8,22 @@ setuptools.setup(
     version="0.1",
     author="Philippe Fremy",
     author_email="phil.fremy@free.fr",
-    description="Conversion of mailbox in maildir format to mbox format",
+    description="Conversion of a mailbox in the maildir format to the mbox format",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bluebird75/maildir2mbox",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    py_modules=['maildir2mbox'],
     python_requires='>=3.5',
+    
+    license="Public Domain",
+    keywords="maildir mbox",
+    classifiers=[
+        "Topic :: Communications :: Email",
+        "License :: Public Domain",
+
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+    ],
 )
